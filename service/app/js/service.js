bc82777044569effe097bb8785a33630f7a4c9ad
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-10 06:55:03 +1000

    Added sample Cache-Control headers to service

--- service/app/js/service.js
+++ service/app/js/service.js
@@ -9,39 +9,46 @@ app.configure(function () {
   app.use(express.methodOverride());
   app.use(app.router);
   app.enable('trust proxy');
-
-  //app.use('/', express.static('app/static')); // Static files
 });
 
 var location = 'app/static';
 
 // Serve .html and .htm files
 app.get(/.*\.html?/, function (request, response) {
+//  sleep.usleep(500 * 1000); // microseconds
+  response.setHeader('Cache-Control', 'no-cache');
   response.sendfile(location + request.path);
 });
 
 // Serve .css files
 app.get(/.*\.css/, function (request, response) {
+//  sleep.usleep(500 * 1000); // microseconds
+  response.setHeader('Cache-Control', 'no-cache');
   response.sendfile(location + request.path);
 });
 
 // Serve .js files
 app.get(/.*\.js/, function (request, response) {
+//  sleep.usleep(500 * 1000); // microseconds
+  response.setHeader('Cache-Control', 'no-cache');
   response.sendfile(location + request.path);
 });
 
 // Serve .png files
 app.get(/.*\.png/, function (request, response) {
 //  sleep.usleep(500 * 1000); // microseconds
+  response.setHeader('Cache-Control', 'no-cache');
   response.sendfile(location + request.path);
 });
 
 // Serve .ico files
 app.get(/.*\.ico/, function (request, response) {
+//  sleep.usleep(500 * 1000); // microseconds
+  response.setHeader('Cache-Control', 'no-cache');
   response.sendfile(location + request.path);
 });
 
-// Hello World
+// Hello World, for experimentation
 app.get('/hello', function (request, response) {
   response.set('Content-Type', 'text/plain');
   response.send('Hello World');
@@ -50,4 +57,4 @@ app.get('/hello', function (request, response) {
 // This section must appear last
 var port = 8000;
 app.listen(port);
-console.log('Service listening on port ' + port);
+console.log('Server listening on port ' + port);
